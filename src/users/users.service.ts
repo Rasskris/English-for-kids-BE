3d59be2c7601fd7355423fcda04f47c1421ba3b3
Rasskris <rassoshenko.kristina@yandex.ru>
2021-10-18 14:49:56 +0300

    EFK-BE-3 fix: fix user processing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { USER_EMAIL_EXEPTION } from 'src/constants';
 import { Repository } from 'typeorm';
+import { USER_EMAIL_EXEPTION, USER_ID_EXEPTION } from '../constants';
 import { CreateUserDto } from './dto/createUser.dto';
-import { User } from './user.entity';
+import { User } from '.';
 
 @Injectable()
 export class UsersService {
@@ -21,9 +21,16 @@ export class UsersService {
     throw new HttpException(USER_EMAIL_EXEPTION, HttpStatus.NOT_FOUND);
   }
 
+  async getById(id: number) {
+    const user = await this.usersRepository.findOne({ id });
+    if (user) {
+      return user;
+    }
+    throw new HttpException(USER_ID_EXEPTION, HttpStatus.NOT_FOUND);
+  }
+
   async create(userData: CreateUserDto) {
     const newUser = await this.usersRepository.create(userData);
-
     await this.usersRepository.save(newUser);
 
     return newUser;
